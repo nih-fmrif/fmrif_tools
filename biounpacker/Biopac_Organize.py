098d62926ee07d5f02eaa2966ccf2b389ef96a5a
Jan Varada <jan.varada@nih.gov>
2017-08-29 19:19:22 -0400

    Refactored biounpacker to support calls as library

--- biounpacker/Biopac_Organize.py
+++ biounpacker/Biopac_Organize.py
@@ -14,11 +14,24 @@ import os
 import bioread
 import matplotlib.pyplot as plt
 
-from common_utils.utils import init_log, log_shutdown
+from common_utils.utils import init_log, log_shutdown, create_path
 from datetime import datetime
 
 
-def main():
+def biounpacker(biopac_file):
+
+    data = bioread.read_file(biopac_file)
+
+    biopac_channels = {
+        'resp': data.channels[0],
+        'ecg': data.channels[1],
+        'triggers': data.channels[2]
+    }
+
+    return biopac_channels
+
+
+if __name__ == "__main__":
 
     parser = argparse.ArgumentParser('Options')
 
@@ -66,16 +79,13 @@ def main():
 
     log.info("Reading biopack file: {}".format(settings.input_file))
 
-    data = bioread.read_file(settings.input_file)
+    biopac_channels = biounpacker(settings.input_file)
 
-    log.info("Parsing biopack file...")
-
-    data_resp = np.array(data.channels[0].data, dtype=float)
-    data_ecg = np.array(data.channels[1].data, dtype=float)
-    data_trigger = np.array(data.channels[2].data, dtype=float)
+    data_resp = np.array(biopac_channels['resp'].data, dtype=float)
+    data_ecg = np.array(biopac_channels['ecg'].data, dtype=float)
+    data_trigger = np.array(biopac_channels['triggers'].data, dtype=float)
 
     if settings.info:
-
         log.info("Plotting physiological data onto file: "
                  "{}_plots.pdf".format(settings.output_prefix))
 
@@ -83,39 +93,39 @@ def main():
         fig.set_size_inches(8.5, 11)
         fig.subplots_adjust(hspace=3, wspace=0)
         plt.subplot(3, 1, 1)
-        plt.title("{} - {} samples/secs".format(data.channels[0].name, data.channels[0].samples_per_second))
-        plt.ylabel(data.channels[0].units)
+        plt.title("{} - {} samples/secs".format(biopac_channels['resp'].name,
+                                                biopac_channels['resp'].samples_per_second))
+        plt.ylabel(biopac_channels['resp'].units)
         plt.xlabel("sample")
         plt.plot(data_resp)
         plt.subplot(3, 1, 2)
-        plt.title("{} - {} samples/secs".format(data.channels[1].name, data.channels[1].samples_per_second))
-        plt.ylabel(data.channels[0].units)
+        plt.title("{} - {} samples/secs".format(biopac_channels['ecg'].name,
+                                                biopac_channels['ecg'].samples_per_second))
+        plt.ylabel(biopac_channels['ecg'].units)
         plt.xlabel("sample")
         plt.plot(data_ecg)
         plt.subplot(3, 1, 3)
-        plt.title("{} - {} samples/secs".format(data.channels[2].name, data.channels[2].samples_per_second))
-        plt.ylabel(data.channels[2].units)
+        plt.title("{} - {} samples/secs".format(biopac_channels['triggers'].name,
+                                                biopac_channels['triggers'].samples_per_second))
+        plt.ylabel(biopac_channels['triggers'].units)
         plt.xlabel("sample")
         plt.plot(data_trigger)
         fig.tight_layout()
         plt.savefig("{}_plots.pdf".format(settings.output_prefix))
         plt.close()
 
-    # Todo: add output directory option
-    # if not os.path.isdir(os.path.dirname(settings.output_prefix)):
-    #     log.info("Creating output directory: {}".format(os.path.dirname(settings.output_prefix)))
-    #     os.mkdir(os.path.dirname(settings.output_prefix))
+    out_path = os.path.abspath(settings.output_prefix)
+
+    if not os.path.isdir(os.path.dirname(out_path)):
+        log.info("Creating output directory: {}".format(os.path.dirname(out_path)))
+        create_path(os.path.dirname(out_path))
 
     log.info("Saving 1D files...")
 
-    np.savetxt(settings.output_prefix + '_Resp.1D', data_resp)
-    np.savetxt(settings.output_prefix + '_ECG.1D', data_ecg)
-    np.savetxt(settings.output_prefix + '_Trigger.1D', data_trigger)
+    np.savetxt(out_path + '_Resp.1D', data_resp)
+    np.savetxt(out_path + '_ECG.1D', data_ecg)
+    np.savetxt(out_path + '_Trigger.1D', data_trigger)
 
     log.info("Biopack data to 1D file conversion complete!")
 
     log_shutdown(log)
-
-
-if __name__ == "__main__":
-    main()
