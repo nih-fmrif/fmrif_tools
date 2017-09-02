7e4df07a26eaaff20091bf3e8f7399221ec7ca3a
Jan Varada <jan.varada@nih.gov>
2017-09-01 21:00:49 -0400

    Modfied RTD config to use Py3 and mock C-dependent libs

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,20 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+# Mock C dependent libraries
+import sys
+from unittest.mock import MagicMock
+
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+
+MOCK_MODULES = ['bioread', 'matplotlib', 'numpy', 'pandas']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 
 # -- General configuration ------------------------------------------------
 
